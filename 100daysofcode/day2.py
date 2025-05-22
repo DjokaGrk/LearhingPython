ae0048c40fcf8721ef163d44497149c3241ac0e9
djokagrk <djokagrk@gmail.com>
2025-05-22 12:44:38 +0200

    day 2 added

---
+++ 100daysofcode/day2.py
@@ -0,0 +1,26 @@
+# print(type(12.23))
+# print(type(True))
+#
+# name = input("enter your name: ")
+# print("Number of letters in your name: " + str(len(name)))
+#
+# print("Number of letters in your name: " + str(len(input("enter your name "))))
+# broj = 12345
+# string = "string"
+# bool = True
+# float = 3.14
+#
+# print(type(broj))
+# print(type(string))
+# print(type(bool))
+# print(type(float))
+#
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
+
+tezina_bmi = print(input("koja je tvoja tezina"))
+visina_bmi = print(input(("koja je tvoja visina")))
+tezina_bmi = int(tezina_bmi)
+bmi = int(tezina_bmi) / int(visina_bmi**2)
+print(round(bmi, 2))
