4bed61897fb650ba387fc015f59356b1a1df1c4a
djokagrk <djokagrk@gmail.com>
2025-05-22 13:17:27 +0200

    dan 2 tip calculator

--- 100daysofcode/day2.py
+++ 100daysofcode/day2.py
@@ -16,9 +16,13 @@
 
 # print(3 * (3 + 3) / 3 - 3)
 
-tezina_bmi = float(input("koja je tvoja tezina "))
-visina_bmi = float(input("koja je tvoja visina "))
-bmi = tezina_bmi / (visina_bmi**2)
-print(f"tvoj bmi je {bmi:.2f}")
+# tezina_bmi = float(input("koja je tvoja tezina "))
+# visina_bmi = float(input("koja je tvoja visina "))
+# bmi = tezina_bmi / (visina_bmi**2)
+# print(f"tvoj bmi je {bmi:.2f}")
 # print(tezina_bmi, visina_bmi)
 # print(f"tvoj bmi je: {tezina_bmi + visina_bmi}")
+wel = "welcome to the tip calculator"
+bill = float(input("koliko je racun "))
+tip = float(input("koliko bi hteo da je tip? 10, 12, 15? "))
+split = float(input("koliko ljudi deli racun "))
