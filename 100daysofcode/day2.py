b41b81e094a53d73ca4f01d5d64a05a3613c907d
djokagrk <djokagrk@gmail.com>
2025-05-23 11:09:52 +0200

    dan 3 added

--- 100daysofcode/day2.py
+++ 100daysofcode/day2.py
@@ -2,8 +2,8 @@
 #
 # name = input("enter your name: ")
 # print("Number of letters in your name: " + str(len(name)))
-#
-# print("Number of letters in your name: " + str(len(input("enter your name "))))
+
+# print("Koliko slova ima tvoje ime: " + str(len(input("enter your name "))))
 # broj = 12345
 # string = "string"
 # bool = True
@@ -13,21 +13,21 @@
 # print(type(string))
 # print(type(bool))
 # print(type(float))
-
+#
 # print(3 * (3 + 3) / 3 - 3)
-
-# tezina_bmi = float(input("koja je tvoja tezina "))
-# visina_bmi = float(input("koja je tvoja visina "))
+#
+# tezina_bmi = int(input("koja je tvoja tezina "))
+# visina_bmi = int(input("koja je tvoja visina "))
 # bmi = tezina_bmi / (visina_bmi**2)
 # print(f"tvoj bmi je {bmi:.2f}")
 # print(tezina_bmi, visina_bmi)
 # print(f"tvoj bmi je: {tezina_bmi + visina_bmi}")
-print("welcome to the tip calculator")
-bill = float(input("koliko je racun? $"))
+# print("welcome to the tip calculator")
+bill_amount = float(input("koliko je racun? $"))
 tip = int(input("koliko bi hteo da je tip? 10, 12, 15? "))
-total_tip = bill * (tip / 100)
-total_bill = bill + total_tip
+total_tip = bill_amount * (tip / 100)
+total_bill = bill_amount + total_tip
 split = int(input("koliko ljudi deli racun "))
 
-racun = round(total_bill / split, 2)
-print(f"treba svako od vas {split} treba dati po {racun} dolara")
+ceo_racun = round(total_bill / split, 2)
+print(f"treba svako od vas {split} treba dati po {ceo_racun} dolara")
