1b3330f303af61064bba2b7230b15111f1c33a56
djokagrk <djokagrk@gmail.com>
2025-05-23 12:44:53 +0200

    dodan pizza delive program

---
+++ 100daysofcode/day3.py
@@ -0,0 +1,71 @@
+# print("wellcome to rollercoster")
+# height = int(input("what is your height in cm? "))
+# age = int(input("koliko imas godina? "))
+#
+# if height >= 120:
+#     if age <= 12:
+#         print("You can ride the rollercoster")
+#         bill = 7
+#     elif age <= 18:
+#         print("You can ride the rollercoster")
+#         bill = 10
+#     else:
+#         print("You can ride the rollercoster")
+#         bill = 13
+#
+#     photo = input("do u wonna photo say y for YES or n for NO: ")
+#     if photo == "y":
+#         bill += 3  # dodaje 3 pare za bill
+#     print(f"Your total bill is {bill} $")
+# else:
+#     print("jedi muda nema vozenje")
+
+# print("welcome to EvenOrOdd.py number guesser :)")
+#
+# num = int(input("upisi broj? "))
+#
+# if num % 2 == 0:
+#     print("number is even")
+# else:
+#     print("number is odd")
+
+# weight = 85
+# height = 1.85
+#
+# bmi = weight / (height**2)
+#
+# # 🚨 Do not modify the values above
+# # Write your code below 👇
+#
+# if bmi < 18.5:
+#     print("underweight")
+# elif bmi < 25:
+#     print("normal weight")
+# else:
+#     print("overweight")
+
+print("welcome to piza delivery")
+# smallPizza= 15
+# meddiumPizza = 20
+# largePizza = 25
+
+size = input("what size do u wonna take: s, m , l? ")
+pepperoni = input("do u wonna pepperoni? y or n? ")
+cheese = input("do you wonna extra cheese? y or n? ")
+
+# pizza size
+if size == "s":
+    bill = 15
+    if pepperoni == "y":
+        bill += 2
+elif size == "m":
+    bill = 20
+    if pepperoni == "y":
+        bill += 3
+else:
+    bill = 25
+    if pepperoni == "y":
+        bill += 3
+if cheese == "y":
+    bill += 1
+print(f"your total bill is {bill}$")
