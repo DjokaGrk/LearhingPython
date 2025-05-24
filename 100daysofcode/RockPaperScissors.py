8a036a871060c49735288e0909467076a6c17b4d
djokagrk <djokagrk@gmail.com>
2025-05-24 20:56:50 +0200

    uradjena igra

--- 100daysofcode/RockPaperScissors.py
+++ 100daysofcode/RockPaperScissors.py
@@ -1,11 +1,5 @@
-paper = """
- _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-"""
+import random
+
 rock = """
     _______
 ---'   ____)
@@ -13,6 +7,14 @@ rock = """
       (_____)
       (____)
 ---.__(___)
+"""
+paper = """
+ _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
 """
 scissors = """
     _______
@@ -22,3 +24,39 @@ scissors = """
       (____)
 ---.__(___)
 """
+choice = int(
+    input("What do you choose? Type 0 for Rock,1 for Paper or 2 for Scissors.\n")
+)
+choices = [rock, paper, scissors]
+cpu_choice = random.randint(0, 2)
+# prikaz slika
+if choice == 0:
+    print("you choose")
+    print(rock)
+elif choice == 1:
+    print("you choose")
+    print(paper)
+else:
+    print("you choose")
+    print(scissors)
+if cpu_choice == 0:
+    print("cpu choose")
+    print(rock)
+elif cpu_choice == 1:
+    print("cpu choose")
+    print(paper)
+else:
+    print("cpu choose")
+    print(scissors)
+
+# ko pobedjuje
+if choice == cpu_choice:
+    print("its a draw")
+elif (
+    (choice == 0 and cpu_choice == 2)
+    or (choice == 1 and cpu_choice == 0)
+    or (choice == 2 and cpu_choice == 1)
+):
+    print("you win")
+else:
+    print("you lose")
