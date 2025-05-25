c75f3c3dba499bce59256abceaa8970400bbbe6e
djokagrk <djokagrk@gmail.com>
2025-05-25 08:33:56 +0200

    dodan invalid number

--- 100daysofcode/RockPaperScissors.py
+++ 100daysofcode/RockPaperScissors.py
@@ -28,21 +28,20 @@ choice = int(
     input("What do you choose? Type 0 for Rock,1 for Paper or 2 for Scissors.\n")
 )
 choices = [rock, paper, scissors]
-cpu_choice = random.randint(0, 2)
-
-print(f"\nyou choose\n{choices[choice]}")
-print(f"\ncpu choose\n{choices[cpu_choice]}")
-
-# ko pobedjuje
 if choice < 0 or choice >= 3:
     print("invalid number , try again")
-elif choice == cpu_choice:
-    print("its a draw")
-elif (
-    (choice == 0 and cpu_choice == 2)
-    or (choice == 1 and cpu_choice == 0)
-    or (choice == 2 and cpu_choice == 1)
-):
-    print("you win")
 else:
-    print("you lose")
+    cpu_choice = random.randint(0, 2)
+    print(f"\nyou choose\n{choices[choice]}")
+    print(f"\ncpu choose\n{choices[cpu_choice]}")
+    # ko pobedjuje
+    if choice == cpu_choice:
+        print("its a draw")
+    elif (
+        (choice == 0 and cpu_choice == 2)
+        or (choice == 1 and cpu_choice == 0)
+        or (choice == 2 and cpu_choice == 1)
+    ):
+        print("you win")
+    else:
+        print("you lose")
