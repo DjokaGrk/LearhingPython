d52e26f7e0729057751d4b0bbd2c554ad7e2f56d
djokagrk <djokagrk@gmail.com>
2025-05-26 19:19:51 +0200

    uradjen dan 5 kao password generator

--- 100daysofcode/day5.py
+++ 100daysofcode/day5.py
@@ -1,5 +1,37 @@
-fruits = ["apple", "peach", "pear"]
+# fruits = ["apple", "peach", "pear"]
+#
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
 
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
+brojevi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# max_broj = max(brojevi)
+# print("Najveći broj je:", max_broj)
+
+# max_broj = 0
+# for broj in brojevi:
+#     if broj > max_broj:
+#         max_broj = broj
+# print("Najveći broj je:", max_broj)
+# max broj radi tako sto ide kroz sve brojeve i upoređuje ih sa trenutnim max_broj ,
+# ukoliko je veći od trenutnog max_broj, postavlja ga kao novi max_broj
+# i na taj nacin onaj broj koji ostane na kraju je najveći broj u listi
+
+# range(10) # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#
+# zbir = 0
+# for num in range(1, 101):
+#     zbir += num
+#     print(zbir)
+
+
+# for num in range(1, 101):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("FizzBuzz")
+#     elif num % 3 == 0:
+#         print("Fizz")
+#     elif num % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(num)
---
+++ 100daysofcode/passwordGenerator.py
@@ -0,0 +1,58 @@
+import random
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]
+nesumicna_slova = []
+nesumicni_simboli = []
+nesumicni_brojevi = []
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+if nr_letters <= len(letters):
+    nesumicna_slova = random.sample(letters, nr_letters)
+    # print("Letters:", nesumicna_slova)
+    #
+if nr_symbols <= len(symbols):
+    nesumicni_simboli = random.sample(symbols, nr_symbols)
+    # print("Symbols:", nesumicni_simboli)
+    #
+if nr_numbers <= len(numbers):
+    nesumicni_brojevi = random.sample(numbers, nr_numbers)
+    # print("Numbers:", nesumicni_brojevi)
+password_list = nesumicna_slova + nesumicni_simboli + nesumicni_brojevi
+password_weak = "".join(password_list)
+random.shuffle(password_list)
+# print("Password list:", str(password_list))
+password = "".join(password_list)
+print(f"Your weak password is: {password_weak}")
+print(f"Your strong password is: {password}")
