7438bc7d4b83305084429c0beb673f4e7642d2d7
Djokagrk <djokagrk@gmail.com>
2025-06-01 20:36:16 +0200

    add: implement hurdle 4 solution with jump and turn functions

--- 100daysofcode/day6.py
+++ 100daysofcode/day6.py
@@ -56,3 +56,26 @@
 #     elif front_is_clear() or wall_on_right():
 #         move()
 #     else: turn_right()
+
+# hurdle 4 udemy techer way
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#   turn_left()
+# while wall_in_right():
+#   move()
+# turn_right()
+# move()
+# turn_right()
+# while front_is_clear():
+#   move()
+# turn_left()
+
+# while not at_goal():
+#   if wall_in_front():
+#     jump()
+#   else:
+#     move()
