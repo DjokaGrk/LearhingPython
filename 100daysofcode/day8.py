73e64a4a13996aef7beb80381093d5a9d2692381
djokagrk <djokagrk@gmail.com>
2025-06-09 09:44:24 +0200

    pushed some projects and added some days

---
+++ 100daysofcode/day8.py
@@ -0,0 +1,19 @@
+# def greet():
+#     print("hello"),
+#     print("djoka"),
+#     print("grk")
+# greet()
+
+# def greet_with_name(name):
+#     print(f"hello {name}")
+#
+# greet_with_name("djoka")
+# greet_with_name("joka")
+# greet_with_name("ena")
+# greet_with_name("luka")
+
+# def life_in_weeks(age):
+#     life = (90 - age) * 52
+#     print(f"You have {life} weeks left.")
+# life_in_weeks(42)
+
