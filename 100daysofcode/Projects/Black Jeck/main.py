3a1805e918dac4a8a125b1430e69b634d9a664bb
djokagrk <djokagrk@gmail.com>
2025-06-10 13:29:16 +0200

    rename blackJeck function to black_jeck and restructure game loop logic

--- 100daysofcode/Projects/Black Jeck/main.py
+++ 100daysofcode/Projects/Black Jeck/main.py
@@ -3,7 +3,7 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
-def blackJeck():
+def black_jeck():
     if start == 'y':
         # print(logo)
         player_cards = random.sample(cards, 2)
@@ -13,3 +13,19 @@ def blackJeck():
         print(f"Computer's first card: {cpu_cards[0]}")
     else:
         print("you enter 'n' game will be closed")
+        return
+    while player_score < 21:
+        new_card = input(f"Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+        if new_card == 'y':
+            player_card_new = random.choice(cards)
+            player_cards.append(player_card_new)
+            player_score = sum(player_cards)
+            print(f"Your card: {player_cards}, current score: {player_score}")
+            print(f"Computer's first card: {cpu_cards[0]}")
+        elif player_score > 21:
+            print(f"Your final hand: {player_cards}, final score: {player_score}")
+            print(f"You went over with {player_score}. You lose!")
+            break
+        else:
+            break
+black_jeck()
