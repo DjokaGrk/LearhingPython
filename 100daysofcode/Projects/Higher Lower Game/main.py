0d0ea1f82799c89b179747200460e13bd6a99e70
djokagrk <djokagrk@gmail.com>
2025-06-16 14:48:29 +0200

    refactor: clean up code formatting and improve readability in main game logic

--- 100daysofcode/Projects/Higher Lower Game/main.py
+++ 100daysofcode/Projects/Higher Lower Game/main.py
@@ -2,6 +2,7 @@ from game_data import data
 import random
 from art import logo, vs
 
+
 def get_random_data():
     """
     Returns a random item from the data list.
@@ -10,6 +11,7 @@ def get_random_data():
         dict: A randomly selected dictionary from the data list.
     """
     return random.choice(data)
+
 
 while True:
     """
@@ -31,24 +33,25 @@ while True:
         while item_a == item_b:
             item_b = get_random_data()
 
-        print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}.")
+        print(
+            f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}."
+        )
         print(vs)
-        print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}.")
+        print(
+            f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}."
+        )
 
         choice = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-        if (choice == 'A' and item_a['follower_count'] > item_b['follower_count']) or \
-           (choice == 'B' and item_b['follower_count'] > item_a['follower_count']):
+        if (choice == "A" and item_a["follower_count"] > item_b["follower_count"]) or (
+            choice == "B" and item_b["follower_count"] > item_a["follower_count"]
+        ):
             score += 1
             print(f"You're right! Current score: {score}.")
         else:
             game_continue = False
             print(f"Sorry, that's wrong. Final score: {score}.")
 
-    if input("Do you want to play again? Type 'y' or 'n': ").lower() != 'y':
+    if input("Do you want to play again? Type 'y' or 'n': ").lower() != "y":
         print("Thanks for playing!")
         break
-
-
-
-
