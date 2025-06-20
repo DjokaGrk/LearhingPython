c4777074d93240288d9b394feac8a5744c8191e9
Djokagrk <djokagrk@gmail.com>
2025-06-20 14:11:58 +0200

    clean the code

--- 100daysofcode/Projects/quiz-game-start/main.py
+++ 100daysofcode/Projects/quiz-game-start/main.py
@@ -11,5 +11,5 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-print(f"You've complated quiz your final score")
+print("You've complated quiz your final score")
 print(f"You final score is {quiz.score}/{quiz.question_number}")
