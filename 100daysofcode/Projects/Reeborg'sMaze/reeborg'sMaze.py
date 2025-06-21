8e8037f14fced8b428dd0ee306aa7ab821440331
Djokagrk <djokagrk@gmail.com>
2025-06-21 17:08:07 +0200

    comment the code

--- 100daysofcode/Projects/Reeborg'sMaze/reeborg'sMaze.py
+++ 100daysofcode/Projects/Reeborg'sMaze/reeborg'sMaze.py
@@ -1,21 +1,21 @@
-# Robergeorg's Maze Solver
-def right():
-    turn_left()
-    turn_left()
-    turn_left()
+# # Robergeorg's Maze Solver
+# def right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
 
 
-# move to the first wall
-while front_is_clear():
-    move()
-turn_left()
+# # move to the first wall
+# while front_is_clear():
+#     move()
+# turn_left()
 
-# navigate the maze
-while not at_goal():
-    if right_is_clear():
-        right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+# # navigate the maze
+# while not at_goal():
+#     if right_is_clear():
+#         right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
