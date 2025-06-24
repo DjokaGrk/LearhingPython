5665041f57ac98763299e7b4e0d5d7ba2549c494
djokagrk <djokagrk@gmail.com>
2025-06-24 14:13:18 +0200

    Add Snake class implementation for the Snake Game

--- 100daysofcode/Projects/Snake Game/main.py
+++ 100daysofcode/Projects/Snake Game/main.py
@@ -1,17 +1,29 @@
-from turtle import Screen as S, Turtle as T
+from turtle import Screen as S
+from snake import Snake
+import time
 
 s = S()
 s.setup(width=600, height=600)
 s.bgcolor("black")
 s.title("Snake Game by DjokaGrk")
+s.tracer(0)
 
-segments = []
-start_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
+s.listen()
+s.onkey(snake.up, "Up")
+s.onkey(snake.down, "Down")
+s.onkey(snake.left, "Left")
+s.onkey(snake.right, "Right")
+s.onkey(snake.up, "w")
+s.onkey(snake.down, "s")
+s.onkey(snake.left, "a")
+s.onkey(snake.right, "d")
+# Moving the segmets
+game_is_on = True
+while game_is_on:
+    s.update()
+    time.sleep(0.1)
+    snake.move()
 
-for pos in start_positions:
-    segment = T(shape="square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(pos)
-    segments.append(segment)
+
 s.exitonclick()
