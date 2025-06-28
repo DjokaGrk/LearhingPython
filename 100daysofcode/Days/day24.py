e36e509fb31d206f9c7c12ea4286d0cc0cae5c70
Djokagrk <djokagrk@gmail.com>
2025-06-28 09:09:43 +0200

    Implement mail merge functionality by reading names and letters, creating personalized output files.

--- 100daysofcode/Days/day24.py
+++ 100daysofcode/Days/day24.py
@@ -1,5 +1,5 @@
 # with open("my_file.txt") as file:
 #     content = file.read()
 #     print(content)
-with open("my_file.txt", "a") as file:
+with open("../Days/Text_files/my_file.txt", "a") as file:
     file.write("New content added to the file.\n")
---
+++ 100daysofcode/Projects/Mail Merge Project Start/main.py
@@ -0,0 +1,18 @@
+# Mail Merge Project
+with open("Input/Letters/starting_letter.txt") as file:
+    content = file.read()
+
+with open("Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+# Create the output directory if it doesn't exist
+import os
+
+if not os.path.exists("Output/ReadyToSend"):
+    os.makedirs("Output/ReadyToSend")
+
+for name in names:
+    stripped_name = name.strip()
+    new_letter = content.replace("[name]", stripped_name)
+    with open(f"Output/ReadyToSend/{stripped_name}.txt", "w") as file:
+        file.write(new_letter)
+        print(f"Letter for {stripped_name} created successfully.")
